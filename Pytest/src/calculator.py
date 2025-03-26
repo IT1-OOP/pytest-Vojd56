02f1899985e8760910ed152aad2fb9f1deefe19d
Vojtěch Bláha <90373255+Vojd56@users.noreply.github.com>
2025-03-26 12:22:19 +0100

    26.03

--- Pytest/src/calculator.py
+++ Pytest/src/calculator.py
@@ -1,3 +1,4 @@
+import math
 def add(a, b):
     return a + b
 def add_wrong(a,b):
@@ -12,3 +13,13 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
+
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Cannot take log of non-positive number!")
+    if b <= 0:
+        raise ZeroDivisionError("Cannot take log with non-positive base!")
+    if b == 1:
+        raise NameError("Cannot take log with base 1!")
+    return math.log(a, b)
